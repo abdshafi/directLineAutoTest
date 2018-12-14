67d69ea5882746dbacc0defefcdc53e8caf195b4
Boothiraj Palanisamy <boothiraj.palanisamy@thomsonreuters.com>
2018-12-14 12:01:56 +0000

    Hooks class added.

---
+++ src/test/java/com/blackcat/utilities/Hooks.java
@@ -0,0 +1,40 @@
+package com.blackcat.utilities;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Hooks extends DriverFactory{
+
+    private static final Logger LOG = LoggerFactory.getLogger(Hooks.class);
+
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        LOG.info("###### Starting Scenario : {} ######", scenario.getName());
+    }
+
+
+    @After
+    public void afterScenario(Scenario scenario){
+        if(scenario.isFailed()){
+            embedScreenshot(scenario);
+        }
+    }
+
+    private void embedScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            try {
+                byte[] screenShot = ((TakesScreenshot) DriverFactory.driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenShot, "image/png");
+            } catch (WebDriverException e) {
+
+            }
+        }
+    }
+}
