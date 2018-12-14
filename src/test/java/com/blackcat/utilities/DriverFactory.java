be8d5ae11fa2e07030e69ccd6178a2ce52e33658
Boothiraj Palanisamy <boothiraj.palanisamy@thomsonreuters.com>
2018-12-14 14:47:30 +0000

    Driver class updated.

--- src/test/java/com/blackcat/utilities/DriverFactory.java
+++ src/test/java/com/blackcat/utilities/DriverFactory.java
@@ -1,11 +1,13 @@
 package com.blackcat.utilities;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * Created by bothees on 27/11/2018.
@@ -20,6 +22,12 @@ public class DriverFactory {
 
     TestContext testContext  = TestContext.getInstance();
 
+    private static final String workingDir = System.getProperty("user.dir");
+
+    private static final String workingOS = System.getProperty("os.name").toLowerCase();
+
+    private static ChromeOptions options;
+
 
     public void initialiseDriver() {
         if(driver!=null){
@@ -29,9 +37,13 @@ public class DriverFactory {
     }
 
     private WebDriver getLocalDriver() {
+        if (workingOS.contains("mac")) {
+            options.addArguments("--kiosk");
+        }
         if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            DesiredCapabilities capabilities = getChromeDesiredCapabilities();
             System.setProperty(CHROME_DRIVER, testContext.readproperty("CHROME.DRIVER"));
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(capabilities);
         } else if (System.getProperty("browser").equalsIgnoreCase("firefox")) {
             System.setProperty(FIREFOX_DRIVER, testContext.readproperty("FIREFOX.DRIVER"));
             DesiredCapabilities capabilities = DesiredCapabilities.firefox();
@@ -43,6 +55,27 @@ public class DriverFactory {
 
     public WebDriver getDriver(){
         return driver;
+    }
+
+    private DesiredCapabilities getChromeDesiredCapabilities() {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+
+        HashMap<String, String> chromePreferences = new HashMap();
+        chromePreferences.put("profile.password_manager_enabled", "false");
+        chromePreferences.put("credentials_enable_service", "false");
+        chromePreferences.put("download.default_directory", workingDir + "/downloads");
+        options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePreferences);
+        options.addArguments("--disable-web-security");
+        options.addArguments("--test-type");
+
+        capabilities.setCapability("chrome.switches", Collections.singletonList("--no-default-browser-check"));
+        capabilities.setCapability("chrome.switches", Collections.singletonList("--disable-logging"));
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        capabilities.setCapability("chrome.verbose", false);
+        capabilities.setJavascriptEnabled(true);
+        capabilities.setCapability("nativeEvents", true);
+        return capabilities;
     }
 
 
